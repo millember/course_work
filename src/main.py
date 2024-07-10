7a666891e2407c6e4eca32a9d3491c9e9216ab99
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-07-11 04:39:20 +0500

    доработка

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from src.sorting import sorting
 
 
 def interface():
-
     """Функция для взаимодействия с пользователем"""
 
     user_vacancy = input('Введите вакансию для поиска на сайте hh.ru: \n')
@@ -12,20 +11,20 @@ def interface():
     vacancies = hh.get_vacancies(user_vacancy)
 
     fv = ListVacancies()
-    fv1 = fv.save_vacancies(vacancies)
+    fv.save_vacancies(vacancies)
 
     name_vac = input('Введите название вакансии: \n')
-    fv3 = fv.add_vacancy(name_vac)
+    fv.add_vacancy(name_vac)
 
     name_criterion = input('Введите критерий для отбора вакансий: \n')
-    fv4 = fv.get_data(name_criterion)
+    fv.get_data(name_criterion)
 
     n = input('Введите количество вакансий для просмотра: \n')
-    top_vacancies = sorting(int(n))
+    sorting(int(n))
 
     name_exit = input('Завершим и очистим файл вакансий да/нет : \n')
     if name_exit == 'да':
-        fv4 = fv.delete_vacancy()
+        fv.delete_vacancy()
     else:
         interface()
 
